96e04b44b2129d502b7f0354f07acb9461917e18
Adam Bersanukaev <adambers2020@gmail.com>
2023-12-06 12:18:32 +0300

    add UseSearch component

--- src/pages/DeviceList/DeviceList.js
+++ src/pages/DeviceList/DeviceList.js
@@ -6,12 +6,21 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Box } from "@mui/material";
 import BasicModal from "../../components/Modal/Modal";
 import AddItems from "../../components/AddItems/AddItems";
+import UseSearch from "../../components/UseSearch/UseSearch";
 
 export default function DeviceList() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [selectedObjects, setSelectedObjects] = useState([]);
+
+  console.log(selectedObjects);
+
+  const handleSelectionChange = (event, value) => {
+    setSelectedObjects(value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -52,7 +61,14 @@ export default function DeviceList() {
 
   return (
     <>
-      <AddItems />
+      <Box
+        display="flex"
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <UseSearch data={data} onChange={handleSelectionChange} />
+        <AddItems />
+      </Box>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -65,7 +81,7 @@ export default function DeviceList() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((row) => (
+            {selectedObjects.map((row) => (
               <TableRow
                 key={row.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
